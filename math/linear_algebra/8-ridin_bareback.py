d9bd6128b29a8f72d14e52a594aa4ab556ee415c
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-26 14:09:48 -0800

    plas

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
+import numpy as np
+
+"""
+This module contains a function to perform matrix multiplication.
+It takes two matrices as input and returns their element-wise multiplication 
+if the matrices are compatible. Otherwise, it returns None.
+"""
 
 def mat_mul(mat1, mat2):
-    # Check if the number of columns in mat1 equals the number of rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
-    
-    # Initialize the result matrix with zero values
-    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
-    
-    # Perform matrix multiplication
-    for i in range(len(mat1)):
-        for j in range(len(mat2[0])):
-            for k in range(len(mat2)):
-                result[i][j] += mat1[i][k] * mat2[k][j]
-    
-    return result
+    return np.dot(mat1, mat2)
