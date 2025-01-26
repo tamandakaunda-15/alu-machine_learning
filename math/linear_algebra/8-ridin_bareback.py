e7662808b516c6aadf9bdef3cd4c4f60c1662cb7
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-26 14:17:56 -0800

    pass

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-import numpy as np
 
 def mat_mul(mat1, mat2):
-    # Ensure that the number of columns in mat1 equals the number of rows in mat2
+    # Check if multiplication is possible (mat1 columns == mat2 rows)
     if len(mat1[0]) != len(mat2):
         return None
-    # Perform matrix multiplication using numpy's dot function
-    return np.dot(mat1, mat2)
+    
+    # Initialize the result matrix with zeroes
+    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+    
+    # Perform matrix multiplication
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    
+    return result
