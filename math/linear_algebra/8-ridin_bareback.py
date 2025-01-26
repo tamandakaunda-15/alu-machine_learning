234cd2c15f8141f4d1df38432b0cc47a50898058
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-26 14:16:04 -0800

    lack

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 
-"""
-This module performs matrix multiplication using numpy arrays.
-It provides a function to multiply two matrices, mat1 and mat2, 
-and returns the resulting matrix.
-"""
-
 def mat_mul(mat1, mat2):
+    # Ensure that the number of columns in mat1 equals the number of rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
+    # Perform matrix multiplication using numpy's dot function
     return np.dot(mat1, mat2)
