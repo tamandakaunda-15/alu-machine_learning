dc1a872cd63c177c6a1b453348ad0de35745b761
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-26 13:46:05 -0800

    added

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -14,7 +14,7 @@ def cat_arrays(arr1, arr2):
         arr2 (list of int/float): The second array.
 
     Returns:
-        list: A new list containing elements of arr1 followed 
+        list: A new list containing elements of arr1 followed
         by elements of arr2.
     """
     return arr1 + arr2
