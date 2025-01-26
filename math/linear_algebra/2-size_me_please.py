cbb4b093e4ec73a22e2ef0df3b7300eeed56f593
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-26 12:55:46 -0800

    passed

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This module contains a function to calculate the shape of a matrix.
+"""
+
+def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of integers."""
 def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
