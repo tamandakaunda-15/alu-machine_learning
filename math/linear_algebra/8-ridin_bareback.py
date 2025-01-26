bb575931fb20551ffc46a22272def9f9b2e91722
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-26 14:01:06 -0800

    pass

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
-def mat_mul(mat1, mat2):
-    # Check if the number of columns in mat1 equals the number of rows in mat2
-    if len(mat1[0]) != len(mat2):
+"""
+This module provides a function to perform element-wise multiplication of two
+matrices.
+"""
+
+def multiply_matrices(mat1, mat2):
+    """
+    Multiplies two matrices element-wise.
+
+    Args:
+        mat1 (list of lists of int/float): The first matrix.
+        mat2 (list of lists of int/float): The second matrix.
+
+    Returns:
+        list: A new matrix containing the element-wise product of mat1 and mat2.
+        None: If the matrices have different shapes.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    
-    # Initialize the result matrix with zero values
-    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
-    
-    # Perform matrix multiplication
-    for i in range(len(mat1)):
-        for j in range(len(mat2[0])):
-            for k in range(len(mat2)):
-                result[i][j] += mat1[i][k] * mat2[k][j]
-    
-    return result
+
+    return [[mat1[i][j] * mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
