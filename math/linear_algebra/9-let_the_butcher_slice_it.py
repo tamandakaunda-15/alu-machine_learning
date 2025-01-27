707029965a655a37f547952fd14b873b3b50e6bf
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-27 13:14:03 -0800

    passd

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-import numpy as np
-matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
-                   [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
-mat1 = matrix[1:3, :]
-mat2 = matrix[:, 2:4]
-mat3 = matrix[1:4, 2:5]
-print("The middle two rows of the matrix are:\n{}".format(mat1))
-print("The middle two columns of the matrix are:\n{}".format(mat2))
-print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
+
+"""
+This module provides a function to calculate the shape of a numpy.ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a given numpy.ndarray.
+
+    Args:
+        matrix: A numpy.ndarray whose shape is to be determined.
+
+    Returns:
+        tuple: A tuple of integers representing the shape of the matrix.
+    """
+    return matrix.shape
