bda5714433d8c866db023dd7f37d68f5cf16b24b
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-27 13:23:40 -0800

    plus

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -3,13 +3,8 @@ import numpy as np
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
 
-# Get the middle two rows
 mat1 = matrix[1:3, :]
-
-# Get the middle two columns
 mat2 = matrix[:, 2:4]
-
-# Get the bottom-right square 3x3 matrix
 mat3 = matrix[1:4, 3:6]
 
 print("The middle two rows of the matrix are:\n{}".format(mat1))
