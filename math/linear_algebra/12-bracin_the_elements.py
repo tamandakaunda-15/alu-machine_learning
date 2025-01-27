7cbdb4418aa321afe34c2ba33ebe110f83a39747
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-27 12:40:27 -0800

    passed

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python3
 import numpy as np
-np_elementwise = __import__('12-bracin_the_elements').np_elementwise
 
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-
-print(mat1)
-print(mat2)
-
-add, sub, mul, div = np_elementwise(mat1, mat2)
-print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
-
-add, sub, mul, div = np_elementwise(mat1, 2)
-print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
+def np_elementwise(mat1, mat2):
+    """
+    Performs element-wise addition, subtraction, multiplication, and division
+    of two matrices (or a matrix and a scalar).
+    
+    Args:
+        mat1 (numpy.ndarray): The first input matrix.
+        mat2 (numpy.ndarray): The second input matrix or scalar.
+    
+    Returns:
+        tuple: A tuple containing the element-wise sum, difference, product, and quotient.
+    """
+    add = np.add(mat1, mat2)
+    sub = np.subtract(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
+    div = np.divide(mat1, mat2)
+    
+    return add, sub, mul, div
