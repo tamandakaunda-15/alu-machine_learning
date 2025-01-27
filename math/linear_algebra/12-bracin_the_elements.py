411d74302758686977f1c2170cae41a863c1f2c3
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-27 12:46:00 -0800

    passed

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
-
-import numpy as np
-
 def np_elementwise(mat1, mat2):
     """
     Performs element-wise addition, subtraction, multiplication, and division
     of two matrices (or a matrix and a scalar).
-    
+
     Args:
-        mat1 (numpy.ndarray): The first input matrix.
-        mat2 (numpy.ndarray): The second input matrix or scalar.
-    
+        mat1: The first input matrix (numpy-compatible array or scalar).
+        mat2: The second input matrix or scalar.
+
     Returns:
         tuple: A tuple containing the element-wise sum, difference, product, and quotient.
     """
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
-
-    return add, sub, mul, div
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    
+    return add, sub, mul, div                                                                                   
 ~                                                                                                                                                                     
 ~                                                                                                                                                                     
 ~                                                                                                                                                                     
@@ -27,6 +24,4 @@ def np_elementwise(mat1, mat2):
 ~                                                                                                                                                                     
 ~                                                                                                                                                                     
 ~                                                                                                                                                                     
-~                                                                                                                                                                     
-~                                                                                                                                                                     
-~                                                              
+~                                                                                  
