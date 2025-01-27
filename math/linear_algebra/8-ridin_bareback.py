e5f568acf128a3138ad04b27c0e9cd796b9473aa
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-27 13:31:48 -0800

    passed

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
-
 def mat_mul(mat1, mat2):
-    # Check if multiplication is possible (mat1 columns == mat2 rows)
     if len(mat1[0]) != len(mat2):
         return None
-    
-    # Initialize the result matrix with zeroes
-    result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
-    
-    # Perform matrix multiplication
-    for i in range(len(mat1)):
-        for j in range(len(mat2[0])):
-            for k in range(len(mat2)):
-                result[i][j] += mat1[i][k] * mat2[k][j]
-    
+    result = [[sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)] for row in mat1]
     return result
