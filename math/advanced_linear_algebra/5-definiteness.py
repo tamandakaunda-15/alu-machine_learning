5c0de633903b4c7f262351565644f64b603ac500
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-01-31 14:47:56 -0800

    plas

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,15 +1,26 @@
-#!/usr/bin/env python3
+"""
+This module contains a function to determine the definiteness of a matrix.
+
+A matrix can be classified as:
+- Positive definite
+- Positive semi-definite
+- Negative definite
+- Negative semi-definite
+- Indefinite
+
+It uses eigenvalues of the matrix to make this determination.
+"""
+
 import numpy as np
+
 
 def definiteness(matrix):
     """Determines the definiteness of a matrix."""
     # Validate input
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.shape[0] != matrix.shape[1]:
-        return None
-    if matrix.size == 0:
-        return None
+    if matrix.size == 0 or matrix.shape[0] != matrix.shape[1]:
+        return None  # Invalid input: return None
     
     # Compute eigenvalues
     eigenvalues = np.linalg.eigvals(matrix)
