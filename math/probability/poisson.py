f36988cc08a6ff4d9ea39a6b1575941dcaf6505c
tamandakanda-15 <tamandakanda900@gmail.com>
2025-02-26 01:56:35 -0800

    plas

---
+++ math/probability/poisson.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Module for Poisson distribution."""
+
+class Poisson:
+    """Represents a Poisson distribution."""
+    
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes the Poisson distribution.
+        
+        Args:
+            data (list, optional): List of data points to estimate lambtha.
+            lambtha (float, optional): Expected number of occurrences. Defaults to 1.
+        
+        Raises:
+            ValueError: If lambtha is not a positive value.
+            TypeError: If data is not a list.
+            ValueError: If data does not contain multiple values.
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
