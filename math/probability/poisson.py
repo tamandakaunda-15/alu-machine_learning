91d969b24cf44c1a4eae105ec62190db2957e7f6
tamandakanda-15 <tamandakanda900@gmail.com>
2025-02-26 02:33:59 -0800

    plass

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import math
+
 """Module for Poisson distribution."""
 
 
@@ -28,4 +30,21 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))  # Removed trailing whitespace here
+            self.lambtha = float(sum(data) / len(data))
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”."""
+        # If k is not an integer, convert it to an integer
+        if not isinstance(k, int):
+            k = int(k)
+        
+        # If k is out of range (negative), return 0
+        if k < 0:
+            return 0
+        
+        # Calculate the CDF using the formula for Poisson distribution
+        cdf_value = 0
+        for i in range(k + 1):
+            cdf_value += (self.lambtha ** i) * math.exp(-self.lambtha) / math.factorial(i)
+        
+        return cdf_value
