1ddf935421397f9d95a43823a86d2f3bf6a9bbbc
tamandakanda-15 <tamandakanda900@gmail.com>
2025-02-26 08:19:40 -0800

    psdd

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -39,34 +39,11 @@ class Poisson:
             result *= i
         return result
 
-    def pmf(self, k):
-        """Calculates the probability mass function (PMF) for a given k."""
-        if k < 0:
-            return 0
-        k = int(k)
-        pmf_value = (self.lambtha ** k * self.factorial(k)) / \
-                    (2.7182818285 ** self.lambtha)
-        return round(pmf_value, 10)
-
-    def cdf(self, k):
-        """Calculates the cumulative distribution function (CDF) for a given k."""
-        if k < 0:
-            return 0
-        k = int(k)
-        cdf_value = sum(self.pmf(i) for i in range(k + 1))
-        return round(cdf_value, 10)
-
-        result = 1
-        for i in range(2, n + 1):
-            result *= i
-        return result
-
     def exp(self, x):
         """Calculates the exponential of a number (e^x)."""
         result = 1
         term = 1
         n = 1
-        # Use series expansion to approximate e^x
         while True:
             term *= x / n
             result += term
@@ -85,12 +62,17 @@ class Poisson:
         return round(pmf_value, 10)
 
     def cdf(self, k):
-        """Calculates the value of the CDF for a given number of 'successes'."""
+        """Calculates the value of the CDF for a given number of 'successes'.
+
+        Args:
+            k (int): Number of occurrences.
+
+        Returns:
+            float: The cumulative probability up to k.
+        """
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
-        cdf_value = 0
-        for i in range(k + 1):
-            cdf_value += (self.lambtha ** i) * self.exp(-self.lambtha) / self.factorial(i)
+        cdf_value = sum(self.pmf(i) for i in range(k + 1))
         return round(cdf_value, 10)
