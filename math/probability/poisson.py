001eeaf8c8df82abe3f50f864eaacfbd17dc802b
tamandakanda-15 <tamandakanda900@gmail.com>
2025-02-26 09:35:27 -0800

    plas

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -6,7 +6,7 @@
 class Poisson:
     """Represents a Poisson distribution."""
 
-    def __init__(self, data=None, lambtha=1.0):
+    def __init__(self, data=None, lambtha=1.):
         """Initializes the Poisson distribution.
 
         Args:
@@ -29,46 +29,3 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
-
-    def factorial(self, n):
-        """Calculates the factorial of a number."""
-        if n == 0 or n == 1:
-            return 1
-        result = 1
-        for i in range(2, n + 1):
-            result *= i
-        return result
-
-    def exp(self, x):
-        """Calculates the exponential of a number (e^x)."""
-        result = 1
-        term = 1
-        n = 1
-        while True:
-            term *= x / n
-            result += term
-            n += 1
-            if abs(term) < 1e-10:  # Convergence threshold
-                break
-        return result
-
-    def pmf(self, k):
-        """Calculates the value of the PMF for a given
- number of 'successes'."""
-        if not isinstance(k, int):
-            k = int(k)
-        if k < 0:
-            return 0
-        pmf_value = (self.lambtha ** k) * self.exp(-self.lambtha)
- / self.factorial(k)
-        return float("{:.10f}".format(pmf_value))
-
-    def cdf(self, k):
-        """Calculates the value of the CDF for a given number
- of 'successes'."""
-        if not isinstance(k, int):
-            k = int(k)
-        if k < 0:
-            return 0
-        cdf_value = sum(self.pmf(i) for i in range(k + 1))
-        return float("{:.10f}".format(cdf_value))
