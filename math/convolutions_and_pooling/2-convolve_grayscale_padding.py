fa4ede4d93bd67d8269fcf3aaab742daed18c875
tamandakanda-15 <tamandakanda900@gmail.com>
2025-03-14 14:30:12 -0700

    pass

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,34 +1,39 @@
-"""
-Module for applying convolution to grayscale images with padding.
-"""
+#!/usr/bin/env python3
+"""Performs convolution on grayscale images with custom padding"""
 import numpy as np
 
-def convolve_grayscale_padding(images, kernel, padding):
-    """
-    Performs a convolution on grayscale images with padding.
 
-    Parameters:
-    images (numpy.ndarray): Input grayscale images of shape (m, h, w)
-    kernel (numpy.ndarray): Kernel/filter for convolution of shape (kh, kw)
-    padding (tuple): (ph, pw) padding for height and width
+def convolve_grayscale_padding(images, kernel, padding):
+    """Performs a convolution on grayscale images with custom padding
+
+    Args:
+        images (numpy.ndarray): Grayscale images of shape (m, h, w)
+        kernel (numpy.ndarray): Kernel for convolution of shape (kh, kw)
+        padding (tuple): Padding (ph, pw)
 
     Returns:
-    numpy.ndarray: The convolved images
+        numpy.ndarray: The convolved images
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
     ph, pw = padding
-    
-    padded_images = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw)), mode='constant'
-    )
-    new_h = h + 2 * ph - kh + 1
-    new_w = w + 2 * pw - kw + 1
-    output = np.zeros((m, new_h, new_w))
-    
-    for i in range(new_h):
-        for j in range(new_w):
-            region = padded_images[:, i:i+kh, j:j+kw]
-            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
-    
-    return output
+
+    # Compute output dimensions
+    h_new = h + 2 * ph - kh + 1
+    w_new = w + 2 * pw - kw + 1
+
+    # Pad images
+    images_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+
+    # Initialize output
+    convolved = np.zeros((m, h_new, w_new))
+
+    # Perform convolution
+    for i in range(h_new):
+        for j in range(w_new):
+            convolved[:, i, j] = np.sum(
+                images_padded[:, i:i + kh, j:j + kw] * kernel,
+                axis=(1, 2)
+            )
+
+    return convolved
