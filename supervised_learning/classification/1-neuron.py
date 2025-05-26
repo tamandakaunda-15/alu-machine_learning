43b30e57f3408ab278d896bd58568021fb63e0a2
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-05-26 07:33:44 +0200

    passs

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Neuron module that defines a single neuron for binary classification (private attributes)"""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """
+        Initialize a Neuron
+
+        Args:
+            nx (int): number of input features
+
+        Raises:
+            TypeError: if nx is not an integer
+            ValueError: if nx is less than 1
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
