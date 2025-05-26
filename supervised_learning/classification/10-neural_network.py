1f3587184f294de53bc8a4e7506dbeca04b50315
tamandakaunda-15 <tamandakaunda900@gmail.com>
2025-05-26 08:02:22 +0200

    pass

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -1,36 +1,25 @@
 #!/usr/bin/env python3
-"""NeuralNetwork with one hidden layer performing binary
-classification (with forward propagation)"""
 import numpy as np
 
-
 class NeuralNetwork:
-    """Defines a neural network with one hidden layer"""
-
     def __init__(self, nx, nodes):
-        # Validate nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        # Validate nodes
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        # Private hidden layer parameters
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
 
-        # Private output layer parameters
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
-    # Getter methods
     @property
     def W1(self):
         return self.__W1
@@ -57,22 +46,19 @@ class NeuralNetwork:
 
     def forward_prop(self, X):
         """
-        Calculates the forward propagation of the neural network
+        Perform forward propagation using sigmoid activation.
 
         Parameters:
-        - X: numpy.ndarray with shape (nx, m) input data
+        X (numpy.ndarray): Input data of shape (nx, m)
 
-        Updates __A1 and __A2 with activated outputs
-        Returns: __A1, __A2
+        Returns:
+        tuple: Activated outputs of the hidden layer and output
+        neuron (__A1, __A2)
         """
-        # Linear combination for hidden layer
-        Z1 = np.matmul(self.__W1, X) + self.__b1
-        # Sigmoid activation for hidden layer
-        self.__A1 = 1 / (1 + np.exp(-Z1))
+        z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
 
-        # Linear combination for output neuron
-        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
-        # Sigmoid activation for output neuron
-        self.__A2 = 1 / (1 + np.exp(-Z2))
+        z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
 
         return self.__A1, self.__A2
