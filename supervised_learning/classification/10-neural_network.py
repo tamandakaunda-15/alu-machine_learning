193c6368e728c8a108a7912059f72ac00e4ee493
Tamanda Kaunda <mac@MandysQueen.local>
2025-06-13 14:56:06 +0200

    pass

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -1,25 +1,53 @@
 #!/usr/bin/env python3
+"""NeuralNetwork with one hidden layer performing binary classification (forward propagation added)"""
 import numpy as np
 
+
 class NeuralNetwork:
+    """Defines a neural network with one hidden layer"""
+
     def __init__(self, nx, nodes):
+        # Validate nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+
+        # Validate nodes
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
+        # Private hidden layer parameters
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
 
+        # Private output layer parameters
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
+    def sigmoid(self, Z):
+        """Sigmoid activation function"""
+        return 1 / (1 + np.exp(-Z))
+
+    def forward_prop(self, X):
+        """
+        Calculates forward propagation of the neural network
+        X: input data of shape (nx, m)
+        Returns: __A1, __A2
+        """
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = self.sigmoid(Z1)
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = self.sigmoid(Z2)
+
+        return self.__A1, self.__A2
+
+    # Getter methods
     @property
     def W1(self):
         return self.__W1
@@ -42,23 +70,4 @@ class NeuralNetwork:
 
     @property
     def A2(self):
-        return self.__A2
-
-    def forward_prop(self, X):
-        """
-        Perform forward propagation using sigmoid activation.
-
-        Parameters:
-        X (numpy.ndarray): Input data of shape (nx, m)
-
-        Returns:
-        tuple: Activated outputs of the hidden layer and output
-        neuron (__A1, __A2)
-        """
-        z1 = np.dot(self.__W1, X) + self.__b1
-        self.__A1 = 1 / (1 + np.exp(-z1))
-
-        z2 = np.dot(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1 / (1 + np.exp(-z2))
-
-        return self.__A1, self.__A2
+        return self.__A2:wq
